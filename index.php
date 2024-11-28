4eac2f489e58e16800eb46c112b4ec829535274c
andrielystarosky <andriely.starosky.3@gmail.com>
2024-11-28 19:00:51 -0300

    ajustando para entrega 2

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <title> Guardiões do Rio - Colecionando Ecossistemas </title>
     <link rel="stylesheet" type="text/css" href="css/pagina.css"/>
-    <link rel="stylesheet" type="text/css" href="css/pagina2.css"/>
+    <link rel="stylesheet" type="text/css" href="css/pagina1.css"/>
+    
 </head>
 <body>
 
